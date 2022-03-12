13eb707a74e7c81d7454af008b7cad90a33a300b
HoangTien339 <tiennh21@fpt.edu.vn>
2022-03-12 09:27:56 +0700

    Lecture 3: RegisterServlet & Servlet LifeCycle

--- src/main/java/controllers/HelloServlet.java
+++ src/main/java/controllers/HelloServlet.java
@@ -1,6 +1,9 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private List<Integer> list;
        
     public HelloServlet() {
         super();
@@ -30,5 +34,26 @@ public class HelloServlet extends HttpServlet {
 		HttpServletResponse response
 	) throws ServletException, IOException {
 		doGet(request, response);
+	}
+	
+	public void init() {
+		System.out.println("Init...");
+		this.list = new ArrayList<Integer>();
+	}
+	
+	public void service(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		this.list.add(1);
+		this.list.add(2);
+		this.list.add(3);
+		System.out.println("Service: " + this.list.size());
+		
+		super.service(request, response);
+	}
+	
+	public void destroy() {
+		System.out.println("Destroy...");
 	}
 }
